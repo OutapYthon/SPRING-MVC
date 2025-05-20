2e74d25ba7981db53e1d8bb79553e08a7125d74f
saade <saadelmabrouk18@gmail.com>
2025-05-20 19:40:28 +0100

    adding layout

--- src/main/java/ma/enset/bdccspringensetmvc/BdccSpringEnsetMvcApplication.java
+++ src/main/java/ma/enset/bdccspringensetmvc/BdccSpringEnsetMvcApplication.java
@@ -5,9 +5,10 @@ import ma.enset.bdccspringensetmvc.repository.ProductRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class BdccSpringEnsetMvcApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/ma/enset/bdccspringensetmvc/web/ProductController.java
@@ -0,0 +1,28 @@
+package ma.enset.bdccspringensetmvc.web;
+
+import ma.enset.bdccspringensetmvc.entities.Product;
+import ma.enset.bdccspringensetmvc.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class ProductController {
+    @Autowired
+    private ProductRepository productRepository;
+    @GetMapping("/index")
+    public String index(Model model){
+        List<Product> products = productRepository.findAll();
+        model.addAttribute("productList",products);
+        return "products";
+    }
+    @GetMapping("/delete")
+    public String delete(@RequestParam(name="id") Long id){
+        productRepository.deleteById(id);
+        return "redirect:/index";
+    }
+}
