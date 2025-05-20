eef87fe1c2086d22a5d22af388c01edef0c6bfbe
saade <saadelmabrouk18@gmail.com>
2025-05-20 23:00:28 +0100

    add authentication

--- src/main/java/ma/enset/bdccspringensetmvc/web/ProductController.java
+++ src/main/java/ma/enset/bdccspringensetmvc/web/ProductController.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class ProductController {
     @Autowired
     private ProductRepository productRepository;
-    @GetMapping("/index")
+    @GetMapping("/user/index")
     public String index(Model model){
         List<Product> products = productRepository.findAll();
         model.addAttribute("productList",products);
@@ -25,23 +25,23 @@ public class ProductController {
     }
     @GetMapping("/")
     public String home(){
-        return "redirect:/index";
+        return "redirect:/user/index";
     }
-    @GetMapping("/delete")
+    @GetMapping("/admin/delete")
     public String delete(@RequestParam(name="id") Long id){
         productRepository.deleteById(id);
-        return "redirect:/index";
+        return "redirect:/user/index";
     }
-    @GetMapping("/add")
+    @GetMapping("/admin/add")
     public String add(Model model){
         model.addAttribute("product",new Product());
         return "newProduct";
     }
-    @PostMapping("/saveProduct")
+    @PostMapping("/admin/saveProduct")
     public String saveProduct(@Valid Product product, BindingResult bindingResult, Model model){
         if(bindingResult.hasErrors())
             return "newProduct";
         productRepository.save(product);
-        return "redirect:/index";
+        return "redirect:/user/index";
     }
 }
